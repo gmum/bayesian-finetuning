e9b7cf4acbfdbec947f3b26959eb8a38d389e484
tkusmierczyk <tomasz.kusmierczyk@gmail.com>
2025-12-04 10:31:17 +0100

    Bugfix hyperparameters + add cluster settings

--- run_test_single_gpu.sh
+++ run_test_single_gpu.sh
@@ -1,8 +1,25 @@
+#!/bin/bash -l
+#SBATCH --job-name=test1gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=36
+#SBATCH --gres=gpu:1
+#SBATCH --time=00:01:00
+#SBATCH --account=plgbloraxs-gpu-gh200
+#SBATCH --partition=plgrid-gpu-gh200
+#SBATCH --mem=120G
+#SBATCH --output=test1gpu-%j.out
+#SBATCH --error=test1gpu-%j.err
 
 echo "A single-GPU test script..."
 
+##Specific for the cluster
+#ml ML-bundle/24.06a
+#source .env/bin/activate
+
+
 # CONFIGURATION PARAMETERS:
-LORA_R=2
+LORA_R=4
 TASK="obqa" # obqa, arc-e
 SEED=3407
 
@@ -12,11 +29,11 @@ MODEL="roberta-base"
 # optional parameter EPOCHS
 EPOCHS=4
 
-LEARNING_RATE=-1e-3
-CLS_LEARNING_RATE=-1e-3
+LEARNING_RATE=1e-3
+CLS_LEARNING_RATE=1e-3
 LORA_DROPOUT=-0.0
-LORA_WEIGHT_DECAY=-0.01
-CLASSIFIER_WEIGHT_DECAY=-0.01
+LORA_WEIGHT_DECAY=0.01
+CLASSIFIER_WEIGHT_DECAY=0.01
 DO_LAPLACE=True
 LORA_ALPHA=16
 UNFREEZE_A=False
